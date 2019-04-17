bcca725ff5aa917d596e645d3dd8005fd384fbe6
Avi-141 <32239997+Avi-141@users.noreply.github.com>
2019-04-17 23:31:11 +0530

    Update SmsSender.java

--- Java/SmsSender.java
+++ Java/SmsSender.java
@@ -7,15 +7,15 @@ import com.twilio.type.PhoneNumber;
 public class SmsSender{
     // Find your Account Sid and Auth Token at twilio.com/console
     public static final String ACCOUNT_SID =
-            "AC1f402d36871706ea26044373288ab16d";
+            "";
     public static final String AUTH_TOKEN =
-            "2d8048b8d484fb1d555d910f8306ce7a";
+            "";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-        Message message = Message.creator(new PhoneNumber("+919929078141"), // to
-                        new PhoneNumber("+12018317373"), // from
+        Message message = Message.creator(new PhoneNumber("+"), // to
+                        new PhoneNumber("+"), // from
                         "Ticket Booked?").create();
 
        // System.out.println(message.getSid());
